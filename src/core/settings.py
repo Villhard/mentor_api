58e885d5b71d6c287a02ce9583f36a482a03ae58
Victor Ryabov <vic.b@ckend.ru>
2025-02-15 07:45:17 +0300

    refactor: adjust jwt token lifetimes

--- src/core/settings.py
+++ src/core/settings.py
@@ -30,8 +30,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
 
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
