06ca7f6328ae87a675328f9904c41ba81cdb4d11
Victor Ryabov <vic.b@ckend.ru>
2025-02-15 21:22:59 +0300

    feat: optimize queries

--- src/api/views.py
+++ src/api/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db.models import Prefetch
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
@@ -20,11 +21,15 @@ class RegistrationView(generics.CreateAPIView):
 
 class UserListView(generics.ListAPIView):
     serializer_class = UserListSerializer
-    queryset = User.objects.all()
+    queryset = User.objects.prefetch_related(
+        Prefetch('mentees', queryset=User.objects.only('username'))
+    )
     permission_classes = [IsAuthenticated]
 
 class UserDetailView(generics.RetrieveUpdateAPIView):
-    queryset = User.objects.all()
+    queryset = User.objects.select_related('mentor').prefetch_related(
+        Prefetch('mentees', queryset=User.objects.only('username'))
+    )
     permission_classes = [IsAuthenticated, IsSelf]
 
     def get_serializer_class(self):
